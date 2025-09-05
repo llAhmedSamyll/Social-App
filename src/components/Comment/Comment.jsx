f2342419df4b0c0fefe4d29ee6b85574019ed44d
Ahmed Samy <ahmedsamywezoo@gmail.com>
2025-09-05 05:25:15 +0300

    edit for date

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
+
 
 export default function Comment({ comments }) {
   // console.log(comments?.commentCreator)
   dayjs.extend(relativeTime);
+  dayjs.extend(updateLocale);
+  dayjs.updateLocale("en", {
+  relativeTime: {
+    future: "in %s",
+    past: "%s",
+    s: "now",
+    m: "1m",
+    mm: "%dm",
+    h: "1h",
+    hh: "%dh",
+    d: "1d",
+    dd: "%dd",
+    M: "1mo",
+    MM: "%dmo",
+    y: "1y",
+    yy: "%dy",
+  },
+});
+
+
 
   return (
     <>
